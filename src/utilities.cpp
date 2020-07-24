55055aa55aaa4a8e7fa799ac0d27e739b32d5c60
vcabeli <vincent.cabeli@gmail.com>
2020-07-24 17:27:28 +0200

    Fix unused MASS namespace and inf in progress bar

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -424,7 +424,10 @@ int printProgress(double percentage, double startTime, int prg_numSearchMore) {
     double remaining_time =
         (get_wall_time() - startTime) / percentage * (1 - percentage);
     stringstream sremaining_time;
-    if ((remaining_time > 60) & (!std::isinf(remaining_time))) {
+    if (std::isinf(remaining_time)){
+      remaining_time = 0;
+    }
+    if (remaining_time > 60) {
       int minutes = remaining_time / 60;
       if (minutes > 60) {
         int hours = minutes / 60;
