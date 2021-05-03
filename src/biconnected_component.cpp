e6157b6bc6d064af0194555296ef37c789eb011e
Honghao Li <honghaoli42@gmail.com>
2021-05-03 18:01:22 +0200

    Revert bi-directed edges settings to 6f589cb.
    * Conditioning nodes involving bi-directed edges are now considered
    consistent again.

--- src/biconnected_component.cpp
+++ src/biconnected_component.cpp
@@ -51,6 +51,10 @@ void BiconnectedComponent::setCandidateZ(int x, int y, vector<int>& zi_list) {
     auto set_z = getCandidateZ(x, y);
     auto is_consistent = [this, x, y](int z){
       if (latent_) return true;
+      // For double arrow headed edge (x <-> z), z is considered consistent
+      if ((edges_(x, z).status_prev == 2 && edges_(z, x).status_prev == 2) ||
+          (edges_(y, z).status_prev == 2 && edges_(z, y).status_prev == 2))
+        return true;
       // status is either 0 (not connected) or 2 (z is the child of x or y)
       if (edges_(x, z).status_prev != 1 && edges_(y, z).status_prev != 1)
         return false;
@@ -76,6 +80,10 @@ bool BiconnectedComponent::isConsistent(
     // Not in the consistent set
     if (set_z.find(z) == set_z.end())
       return false;
+    // For double arrow headed edge (x <-> z), z is considered consistent
+    if ((edges_(x, z).status == 2 && edges_(z, x).status == 2) ||
+        (edges_(y, z).status == 2 && edges_(z, y).status == 2))
+      continue;
     // status is either 0 (not connected) or 2 (z is the child of x or y)
     if (edges_(x, z).status != 1 && edges_(y, z).status != 1)
       return false;
